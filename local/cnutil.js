357d83f3e334ce0e8574d00c0309c3d2a4c7b2be
kylinGo <593289488@qq.com>
2019-08-02 00:57:17 +0800

    improved

--- local/cnutil.js
+++ local/cnutil.js
@@ -1,10 +1,12 @@
 var os = require('os');
-var http = require('https');
+var path = require('path');
 var iconv = require('iconv-lite');
-var BufferHelper = require('bufferhelper');
 var fs = require('fs');
 //引用cheerio模块,使在服务器端像在客户端上操作DOM,不用正则表达式
 var cheerio = require('cheerio');
+var request = require('request');
+
+var homedir = os.homedir();
 
 var Program = {
     run: function() {
@@ -17,20 +19,20 @@ var Program = {
     },
     // 下载配置项
     _option: {
-        base: 'http://www.boquge.com',
+        base: 'https://www.boquge.com',
         book: ''
     },
     _isOk: true, // 标志当前章节是否已下载成功
     _list: [], // 小说列表
     _book: '', // 保存书籍名
     _errorLog: '\r\n------------------------------------------\r\n', // 保存错误章节日志
-    _outputDir: '', // 小说导出路径
+    _outputDir: path.join(homedir, "Desktop"), // 小说导出路径
     _init: function() {
         var config = fs.readFileSync('config.xml').toString();
         var $ = cheerio.load(config);
         var bookId = $('.book-id').html();
         var startChapterName = $('.start-chapter').text();
-        this._outputDir = $('.output-dir').text();
+        // this._outputDir = $('.output-dir').text();
 
         var option = this._option;
         option.book = '/book/'+ bookId + '/';
@@ -40,20 +42,38 @@ var Program = {
     // 章节抓取入口
     _entry: function(bookListUrl, startChapterName) {
         var _this = this;
-        var req = http.request(bookListUrl, function(res) {
-            //解决中文编码问题
-            var bufferHelper = new BufferHelper();
-            res.on('data', function(chunk) {
-                bufferHelper.concat(chunk);
+
+        var options = {
+            url: bookListUrl,
+            headers: {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+            }
+        };
+
+        request(options).on('response', function (res) {
+            var chunks = [];
+            res.on('data', function (chunk) {
+                chunks = chunks.concat(chunk);
             });
-            res.on('end', function() {
-                //注意，此编码必须与抓取页面的编码一致，否则会出现乱码，也可以动态去识别
-                var val = iconv.decode(bufferHelper.toBuffer(), 'gbk');
+        
+            res.on('end', function () {
+                var buf = Buffer.concat(chunks);
+                // 转码
+                var val = iconv.decode(buf, 'gbk');
+                // console.log("val", val);
                 var $ = cheerio.load(val);
-                var book = $('h1').text().match(/^[\u4e00-\u9fa5]+/)[0] + ' ' + new Date().Format("yyyy-MM-dd") + '-' + Date.now();
+                var bookName = _this._bookId;
+                console.log($('h1').text())
+                if (!$('h1').text().match(/^[\u4e00-\u9fa5]+/)) {
+                    console.log("抓取书名出错");
+                } else {
+                    bookName = $('h1').text().match(/^[\u4e00-\u9fa5]+/)[0];
+                }
+                var book = bookName + ' ' + new Date().Format("yyyy-MM-dd") + '-' + Date.now();
                 _this._book = book;
                 var $links = $('#chapters-list').find('a');
-
+                console.log($links.length);
+                // return;
                 var list = _this._list;
                 var isStart = false;
                 if(!startChapterName){
@@ -70,11 +90,11 @@ var Program = {
                         list.push(chapter);
                     }
                 });
+                console.log(list);
                 // 调用，让它按队列顺序执行，以免章节错乱
                 _this._excuteSnatchTxt();
             });
         });
-        req.end();
     },
     // 根据列表进行章节的顺序抓取
     _excuteSnatchTxt() {
@@ -107,15 +127,25 @@ var Program = {
     _snatchTxt: function(chapterName, bookUrl) {
         var _this = this;
         var url = _this._option.base + bookUrl;
-        var req = http.request(url, function(res) {
-            //解决中文编码问题
-            var bufferHelper = new BufferHelper();
-            res.on('data', function(chunk) {
-                bufferHelper.concat(chunk);
+
+        var options = {
+            url,
+            headers: {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+            }
+        };
+
+        request(options).on('response', function (res) {
+            var chunks = [];
+            res.on('data', function (chunk) {
+                chunks = chunks.concat(chunk);
             });
-            res.on('end', function() {
-                //注意，此编码必须与抓取页面的编码一致，否则会出现乱码，也可以动态去识别
-                var val = iconv.decode(bufferHelper.toBuffer(), 'gbk');
+        
+            res.on('end', function () {
+                var buf = Buffer.concat(chunks);
+                // 转码
+                var val = iconv.decode(buf, 'gbk');
+                // console.log("val", val);
                 var $ = cheerio.load(val);
                 var text = $('#txtContent').text();
                 if(text.length > 200){
@@ -131,19 +161,15 @@ var Program = {
                     // 重置标志
                     _this._isOk = true;
                 }else{
-                    console.log('----------text为空，重新下载----------');
+                    console.log(chapterName + '----------text为空，重新下载----------');
                     _this._snatchTxt(chapterName, bookUrl);
                 }
             });
-            res.on('error', function(e) {
-                console.log('响应异常', e);
-            });
         });
-        req.end();
     },
     // 将章节写入文件
     _appendTxt: function(txt) {
-        fs.appendFileSync(this._outputDir + this._book + '.txt', txt);
+        fs.appendFileSync(this._outputDir + '\\' + this._book + '.txt', txt);
     },
     // 格式化时间
     _dateFormat: function() {
